8c3c3b923869cd47fe60c0f67a539a0cd38fd053
she4sfalling <shelfi.qrns@gmail.com>
2026-07-07 13:59:40 +0700

    Simpan progress

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ function loginAccount() {
 
     if(username === "" || password==="") {
         alert("Username dan Password tidak boleh kosong!");
+        localStorage.setItem("isLogin", "false");
+    }
+    else {
+        alert("Login Berhasil!");
         localStorage.setItem("isLogin", "true");
+        window.location.href = "index.html";
     }
 }
